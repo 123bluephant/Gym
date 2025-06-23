31a4ca5c1120937793dd2aa3502cc88744aab4d7
lightning-sagar <lightningsagar0@gmail.com>
2025-06-23 21:14:31 +0530

    fix the bug for backend

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,8 +1,18 @@
-import moongose from "mongoose"
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Get correct path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '../.env') });
+import mongoose from 'mongoose';
 
 export const connectdb = async()=>{
     try {
-        await moongose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("connected to db");
     } catch (error) {
         console.log(`error while connecting db${error}`)
